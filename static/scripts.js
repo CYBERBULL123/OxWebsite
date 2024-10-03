036152578d6f677276f5c347f99712202e7d1be7
CYBERBULL123 <opaadi98@gmail.com>
2024-10-03 18:24:56 +0530

    updated landing

--- static/scripts.js
+++ static/scripts.js
@@ -111,3 +111,23 @@ document.addEventListener("DOMContentLoaded", function() {
         retina_detect: true,
     });
 });
+
+
+// Initialize Swiper
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween: 30, // Space between slides
+    effect: 'slide', // Effect type (slide, fade, cube, etc.)
+    loop: true, // Infinite loop mode
+    autoplay: {
+        delay: 2500, // Time interval between slides
+        disableOnInteraction: false, // Don't disable autoplay on user interactions
+    },
+    pagination: {
+        el: '.swiper-pagination', // Pagination element
+        clickable: true, // Make pagination bullets clickable
+    },
+    navigation: {
+        nextEl: '.swiper-button-next', // Next button
+        prevEl: '.swiper-button-prev', // Previous button
+    },
+});
